38bfbe81287f95dc8b0b9fdf726a9ed7c233a3fe
Vera Ueda Pimentel <veraueda@hotmail.com>
2021-05-20 18:29:31 -0300

    testes tijolo

--- TesteHomeTijolo/js/sliderAutomatico.js
+++ TesteHomeTijolo/js/sliderAutomatico.js
@@ -4,11 +4,16 @@ showSlidesX();
 function showSlidesX() {
   var i;
   var slides = document.getElementsByClassName("meusSlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
   setTimeout(showSlidesX, 3000); // Troca a imagem a cada 3 segundos
 }
